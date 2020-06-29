35e35f5600263ac58ff81716ab6d1603d76f209b
Matt Haynie <mrhaynie@live.com>
2020-06-28 22:56:00 -0700

    Added std::array constructor to ImVec4.

--- imgui_desktop/include/imgui_desktop/ImGuiHelpers.h
+++ imgui_desktop/include/imgui_desktop/ImGuiHelpers.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <type_traits>
 
 #define IM_VEC2_CLASS_EXTRA \
@@ -11,6 +12,7 @@
 #define IM_VEC4_CLASS_EXTRA \
 	template<size_t N, typename = std::enable_if_t<N == 4>> \
 	ImVec4(float (&array)[N]) : x(array[0]), y(array[1]), z(array[2]), w(array[3]) {} \
+	ImVec4(const std::array<float, 4>& array) : x(array[0]), y(array[1]), z(array[2]), w(array[3]) {} \
 	ImVec4 operator+(const ImVec4& other) const { return ImVec4(x + other.x, y + other.y, z + other.z, w + other.w); } \
 	ImVec4 operator-(const ImVec4& other) const { return ImVec4(x - other.x, y - other.y, z - other.z, w - other.w); } \
 	ImVec4 operator*(float scalar) const { return ImVec4(x * scalar, y * scalar, z * scalar, w * scalar); }
