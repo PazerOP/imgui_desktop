94f01e555904549111c72d31b6c5a54f9eed8f7d
Matt Haynie <mrhaynie@live.com>
2020-08-13 14:43:01 -0700

    Cleaned up ScopeGuards a bit.

--- imgui_desktop/include/imgui_desktop/ScopeGuards.h
+++ imgui_desktop/include/imgui_desktop/ScopeGuards.h
@@ -1,74 +1,60 @@
 #pragma once
 
+#include <mh/types/disable_copy_move.hpp>
+
 #include <string_view>
 
 enum ImGuiCol_ : int;
 struct ImVec4;
 
-namespace ImGuiDesktop::ScopeGuards
+namespace ImGuiDesktop
 {
-	namespace detail
+	inline namespace ScopeGuards
 	{
-		struct DisableCopy
+		struct ID final : mh::disable_copy_move
 		{
-			DisableCopy() = default;
-			DisableCopy(const DisableCopy&) = delete;
-			DisableCopy& operator=(const DisableCopy&) = delete;
+		public:
+			explicit ID(int int_id);
+			explicit ID(const void* ptr_id);
+			explicit ID(const char* str_id_begin, const char* str_id_end = nullptr);
+			explicit ID(const std::string_view& sv);
+			~ID();
 		};
 
-		struct DisableMove
+		struct StyleColor : mh::disable_copy
 		{
-			DisableMove() = default;
-			DisableMove(DisableMove&&) = delete;
-			DisableMove& operator=(DisableMove&&) = delete;
+			StyleColor() : m_Enabled(false) {}
+			StyleColor(ImGuiCol_ color, const ImVec4& value, bool enabled = true);
+			~StyleColor();
+
+			StyleColor(StyleColor&& other);
+			StyleColor& operator=(StyleColor&& other);
+
+		private:
+			bool m_Enabled = true;
 		};
 
-		struct DisableCopyMove : DisableCopy, DisableMove {};
+		struct TextColor final : StyleColor
+		{
+			TextColor(const ImVec4& color, bool enabled = true);
+		};
+
+		struct GlobalAlpha final : mh::disable_copy_move
+		{
+			explicit GlobalAlpha(float newAlpha);
+			~GlobalAlpha();
+
+		private:
+			float m_OldAlpha;
+		};
+
+		struct Indent final : mh::disable_copy_move
+		{
+			explicit Indent(unsigned count = 1);
+			~Indent();
+
+		private:
+			unsigned m_Count;
+		};
 	}
-
-	struct ID final : detail::DisableCopyMove
-	{
-	public:
-		explicit ID(int int_id);
-		explicit ID(const void* ptr_id);
-		explicit ID(const char* str_id_begin, const char* str_id_end = nullptr);
-		explicit ID(const std::string_view& sv);
-		~ID();
-	};
-
-	struct StyleColor : detail::DisableCopy
-	{
-		StyleColor() : m_Enabled(false) {}
-		StyleColor(ImGuiCol_ color, const ImVec4& value, bool enabled = true);
-		~StyleColor();
-
-		StyleColor(StyleColor&& other);
-		StyleColor& operator=(StyleColor&& other);
-
-	private:
-		bool m_Enabled = true;
-	};
-
-	struct TextColor final : StyleColor
-	{
-		TextColor(const ImVec4& color, bool enabled = true);
-	};
-
-	struct GlobalAlpha final : detail::DisableCopyMove
-	{
-		explicit GlobalAlpha(float newAlpha);
-		~GlobalAlpha();
-
-	private:
-		float m_OldAlpha;
-	};
-
-	struct Indent final : detail::DisableCopyMove
-	{
-		explicit Indent(unsigned count = 1);
-		~Indent();
-
-	private:
-		unsigned m_Count;
-	};
 }
